94fe84aa075e62632411c796152b0e581419dc2e
Alex Mwaniki <112326970+lxmwaniky@users.noreply.github.com>
2023-07-11 22:10:10 +0300

    Add files via upload
    An ASCII art representation of Linux

---
+++ linuc-ascii.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    printf("            .-\"\"\"-.\n");
+    printf("           '       \\\n");
+    printf("          |,.  ,-.  |\n");
+    printf("          |()L( ()| |\n");
+    printf("          |,'  `\".| |\n");
+    printf("          |.___.,'| `\n");
+    printf("         .j `--\"' `  `.\n");
+    printf("        / '        '   \\\n");
+    printf("       / /          `   `.\n");
+    printf("      / /            `    .\n");
+    printf("     / /              l   |\n");
+    printf("    . ,               |   |\n");
+    printf("    ,\"`.             .|   |\n");
+    printf(" _.'   ``.          | `..-'l\n");
+    printf("|       `.`,        |      `.\n");
+    printf("|         `.    __.j         )\n");
+    printf("|__        |--\"\"___|      ,-' \n");
+    printf("   `\"--...,+\"\"\"\"   `._,.-'\n");
+    
+    return 0;
+}
