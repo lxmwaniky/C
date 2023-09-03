6e95225eeb8227054d6c2e433364e250a09e7619
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-03 16:16:43 +0300

    Arrays

--- arrays.c
+++ arrays.c
@@ -2,6 +2,6 @@
 
 int main(){
 	int age[5] = {1,2,3,4,5};
-	printf("%d %d %d %d %d\n", age[0], age[1], age[2], age[3], age[4]);
+	printf("%d %d %d %d %d \n", age[0], age[1], age[2], age[3], age[4]);
 	return 0;
 }
