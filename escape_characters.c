f7af4b3c3ae42d0297160203a394191094df153a
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-16 16:36:51 +0300

    escape_characters.c

---
+++ escape_characters.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - Escape Characters in C
+  *Return: 0
+  */
+int main(void)
+{
+	//printf("Hello World.\nAm Alex"); - New Line
+	//printf("Hello World.\aAm Alex"); - Bell("Beep")
+	//printf("Hello World.\bAm Alex"); - Deletes Previous Character
+	//printf("Hello World.\fAm Alex");
+	//printf("Hello World.\rAm Alex");
+	//printf("Hello World.\tAm Alex");
+	//printf("Hello World.\vAm Alex");
+	//printf("Hello World.\\Am Alex");
+	//printf("Hello World.\?Am Alex");
+	return (0);
+}
