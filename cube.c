20299b3a675d4f52b6294159db12e2eb1efe9e95
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-16 15:43:25 +0300

    Betty Style

--- cube.c
+++ cube.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    int num;
-    printf("Enter a value: ");
-    scanf("%d", &num);
-    int cube = num * num * num;
-    printf("The cube is %d.\n", cube);
-    return 0;
+int main(void)
+{
+	int num;
+
+	printf("Enter a value: ");
+	scanf("%d", &num);
+	int cube = num * num * num;
+
+	printf("The cube is %d.\n", cube);
+	return (0);
 }
