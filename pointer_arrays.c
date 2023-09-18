67b7af1eebe36bbdf42cfe5f9059f184f9976363
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-18 11:33:27 +0300

    pointer_arrays.c

---
+++ pointer_arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  *main - Loops through an array
+  *Return: 0
+  */
+int main(void)
+{
+	int arr[] = {1, 3, 5, 7, 9};
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%p\n", &arr[i]);
+	}
+	return (0);
+}
