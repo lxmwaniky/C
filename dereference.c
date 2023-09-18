710a5ebb66c0a54db69f5ab9fe0efec0fe4c2b15
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-18 11:23:43 +0300

    dereference.c

---
+++ dereference.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - Get value of Variable pointer points to
+  *Return: 0
+  */
+int main(void)
+{
+	int age = 18;
+	int *pAge = &age; /*Stores memory address of var age*/
+
+	printf("Value of age - %d\n", age);
+	printf("Memory Address of age - %p\n", &age);
+	printf("Memory Address of age with Pointer - %p\n", pAge);
+	printf("Value from Memory address Address - %d\n", *pAge);
+	return (0);
+}
