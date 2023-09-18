eddb9c2f2eb5a96ff4149e5e161c04674beafd54
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-18 09:03:15 +0300

    strlen.c

---
+++ strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  *main - Strlen
+  *Return: 0
+  */
+int main(void)
+{
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	printf("%zu", strlen(alphabet));
+	return (0);
+}
