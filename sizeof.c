fe51bb34032398b8c5362aeca422a5ad7ed15ed3
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-18 09:05:17 +0300

    sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+  *main - Sizeof
+  *Return: 0
+  */
+int main(void)
+{
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	printf("%ld", sizeof(alphabet));
+	return (0);
+}
